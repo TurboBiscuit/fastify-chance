b2884062aadd2ebaba725733f2937cfe86479c42
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2020-09-05 00:04:44 -0400

    Fix Types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,11 @@
 import { Chance } from "chance";
+import { FastifyPlugin } from "fastify";
+
 declare module "fastify" {
   interface FastifyInstance {
     chance: Chance.Chance;
   }
 }
+
+declare const fastifyChance: FastifyPlugin;
+export default fastifyChance;
